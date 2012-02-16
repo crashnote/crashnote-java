d02758c8da9e8df8c9563a4d07d2498606ccbcb8
Stephan <stephan.behnke@googlemail.com>
2012-02-16 12:12:56 +0100

    fixed attribution for checksum algorithm

--- core/src/main/java/com/crashnote/core/util/ChksumUtil.java
+++ core/src/main/java/com/crashnote/core/util/ChksumUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011 - 101loops.com <dev@101loops.com>
+ * Copyright (C) - http://code.google.com/p/rabinfingerprint/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
