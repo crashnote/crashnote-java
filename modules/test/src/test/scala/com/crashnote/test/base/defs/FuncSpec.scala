f4ff4f6448b558f9b0223dc10b6fdcb68b5ebcda
Crashnote <dev@crashnote.com>
2013-02-06 20:30:51 +0100

    remove duplicate license header

--- modules/test/src/test/scala/com/crashnote/test/base/defs/FuncSpec.scala
+++ modules/test/src/test/scala/com/crashnote/test/base/defs/FuncSpec.scala
@@ -13,21 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Copyright (C) 2011 - 101loops.com <dev@101loops.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.crashnote.test.base.defs
 
 import org.eclipse.jetty.server.{Request, Server}
@@ -36,7 +21,7 @@ import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import org.specs2.specification.{Fragments, Step}
 import collection.mutable
 import org.apache.commons.io.IOUtils
-import java.io.{InputStream, ByteArrayInputStream}
+import java.io.InputStream
 import java.util.zip.GZIPInputStream
 
 trait FuncSpec
