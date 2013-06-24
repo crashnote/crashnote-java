1dbbb60f20f6afc5a38ff4d075fd3d85225a6d0f
stephanos <github@stephanbehnke.de>
2013-06-24 09:21:19 +0200

    adapted error message

--- servlet/src/main/java/com/crashnote/servlet/CrashnoteFilter.java
+++ servlet/src/main/java/com/crashnote/servlet/CrashnoteFilter.java
@@ -137,8 +137,8 @@ public class CrashnoteFilter
     protected void checkForAppengine() {
         if (isAppEngine())
             throw new RuntimeException("Unsupported Platform! It seems you are developing for / running on " +
-                "Google AppEngine. This library (crashnote-servlet) is not compatible with it - " +
-                "you need the specialized crashnote-appengine. " +
+                "Google AppEngine. This library 'crashnote-servlet' is not compatible with it - " +
+                "you need the specialized 'crashnote-appengine'. " +
                 "Please consult the online docs of Crashnote for further details.");
     }
 
